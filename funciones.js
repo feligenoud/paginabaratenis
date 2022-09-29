7b7a53f74ec83152a0dd1359834338f1fd9d82f0
feligenoud <genoud115@gmail.com>
2022-09-29 18:22:45 -0300

    pagina para entrega29/9

---
+++ funciones.js
@@ -0,0 +1,14 @@
+
+let edad = prompt("Ingrese su edad");
+
+if (edad<0 || parseInt(edad) == NaN) {
+  alert("ingrese una edad valida")
+} else if (edad >=18)
+{
+alert("Bienvenido a Baradero Tenis ")
+}
+else if(edad <18)
+{
+alert("Vas a navegar en el sitio bajo tu responsabilidad")
+}
+
